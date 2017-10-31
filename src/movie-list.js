4487837e3293dd788e17246ad96d8bb720db3517
Alex Pot <info@smartscripts.nl>
2017-10-31 18:21:51 +0100

    Make movies list filterable

--- src/movie-list.js
+++ src/movie-list.js
@@ -1,6 +1,10 @@
+import genres from './util/genres';
+
 module.exports = {
 	template: `<div id="movie-list">
-		<div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+		<div v-for="movie in filteredMovies" class="movie">
+		{{ movie.title }}
+		</div>
 	</div>`,
 
 	data () {
@@ -16,5 +20,23 @@ module.exports = {
 	props: [
 		'genre',
 		'time'
-	]
+	],
+
+	methods: {
+
+		moviePassesGenreFilter (movie) {
+
+			if (!this.genre.length) {
+				return true;
+			}
+
+			return this.genre.find(genre => movie.genre === genre);
+		}
+	},
+
+	computed: {
+		filteredMovies () {
+			return this.movies.filter(this.moviePassesGenreFilter);
+		}
+	}
 };

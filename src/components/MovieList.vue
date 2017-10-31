d551b5b8e76dfd4098c7aa5325c49d600209c205
Alex Pot <info@smartscripts.nl>
2017-10-31 22:48:16 +0100

    Make filters for movies work again

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -24,7 +24,15 @@
 					return true;
 				}
 
-				return this.genre.find(genre => movie.genre === genre);
+				let movieGenres = movie.movie.Genre.split(', ');
+				let matched = true;
+				this.genre.forEach(genre => {
+					if (movieGenres.indexOf(genre) === -1) {
+						matched = false;
+					}
+				});
+
+				return matched;
 			}
 		},
 

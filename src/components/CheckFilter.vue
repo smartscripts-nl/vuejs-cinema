dcf298358bb77febdb51b8e63d8ef9b4609efa44
Alex Pot <info@smartscripts.nl>
2017-11-01 01:41:18 +0100

    Make movies filterable by before or after 6 pm

--- src/components/CheckFilter.vue
+++ src/components/CheckFilter.vue
@@ -1,24 +1,22 @@
 <template>
-	<div :class="{ 'check-filter': true, active: checked }" @click="checkFilter">
-		<span class="checkbox"></span>
-		<span class="check-filter-title">{{ title }}</span>
-	</div>
+    <div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
+        <span class="checkbox"></span>
+        <span class="check-filter-title">{{ title }}</span>
+    </div>
 </template>
-
 <script>
-
-	export default {
-		data () {
-			return {
-				checked: false
-			}
-		},
-		methods: {
-			checkFilter () {
-				this.checked = !this.checked;
-				this.$emit('check-filter', 'genre', this.title, this.checked);
-			}
-		},
-		props: [ 'title' ]
-	};
+    export default {
+        data() {
+            return {
+                checked: false
+            }
+        },
+        props: [ 'title', 'category' ],
+        methods: {
+            checkFilter() {
+                this.checked = !this.checked;
+                this.$emit('check-filter', this.category, this.title, this.checked);
+            }
+        }
+    }
 </script>
--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,20 +1,29 @@
 <template>
 	<div id="movie-filter">
 		<h2>Filter results</h2>
+
+		<h3>By time of day</h3>
 		<div class="filter-group">
-			<check-filter v-for="genre in genres" :title="genre" @check-filter="checkFilter" :key="genre.toUpperCase()"></check-filter>
+			<check-filter v-for="time in times" :title="time" category="time" @check-filter="checkFilter" :key="time.toUpperCase()"></check-filter>
+		</div>
+
+		<h3>By genre</h3>
+		<div class="filter-group">
+			<check-filter v-for="genre in genres" :title="genre" category="genre" @check-filter="checkFilter" :key="genre.toUpperCase()"></check-filter>
 		</div>
 	</div>
 </template>
 
 <script>
 	import genres from '../util/genres';
+	import times from '../util/times';
 	import CheckFilter from './CheckFilter.vue';
 
 	export default {
 		data () {
 			return {
-				genres
+				genres,
+				times
 			}
 		},
 		methods: {
